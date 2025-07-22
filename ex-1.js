b495dd87f9ebba2dd9bca791239b1404643fbce3
Ponlawat <p.sangtub@gmail.com>
2025-07-22 14:30:50 +0700

    refactor: correct callback and higher order function references in exercise comments

--- ex-1.js
+++ ex-1.js
@@ -24,9 +24,9 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 /*
 ====================================
 
-1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function? // forEach
+1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function? // addSalary5000
 
-2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function? // addSalary5000
+2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function? // forEach
 
 ====================================
 */
