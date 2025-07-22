875e385f43e285558711b91dabffcc6da87ba233
83mhpll <dogk678@gmail.com>
2025-07-22 13:55:53 +0700

    refactor: implement forEach and atLeastFive functions with salary and score checks

--- ex-1.js
+++ ex-1.js
@@ -1,26 +1,50 @@
-// Exercise #1: For Each Function
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
-  // Start coding here
+ 
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
-  // Start coding here
+ 
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]));
+  }
+  return result;
 }
 
-// Using `forEach` function here
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
-let newEmployeeSalaries;
+console.log(newEmployeeSalaries); 
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
-/*
-====================================
+const studentScoresRoom1 = [12, 40, 67, 80, 100, 15, 40, 78, 75, 80];
+const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
+const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+function checkGradeOver70(score) {
 
-2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+  return score > 70;
+}
 
-====================================
-*/
+function atLeastFive(array, operation) {
+
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+
+  return count >= 5;
+}
+
+
+const scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+const scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+const scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
+
+console.log(scoreRoom1Result); 
+console.log(scoreRoom3Result); 
+
