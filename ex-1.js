ef47e3aad1a84458a60097ae441a9d49c0ad9ce8
ningnong239 <ningnong2539@gmail.com>
2025-07-22 13:09:19 +0700

    refactor: implement higher-order functions for salary and grade checks

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,22 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]));
+  }
+  return result;
 }
+
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
